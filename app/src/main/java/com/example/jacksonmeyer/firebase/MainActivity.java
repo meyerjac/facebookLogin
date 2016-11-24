356fd72853a83d01caea44bd679734328444e657
jackson Meyer <jacksonmeyer@jacksons-MacBook-Pro.local>
2016-11-24 12:08:01 -0800

    facebook login button appearing, dont know if its connected right

--- app/src/main/java/com/example/jacksonmeyer/firebase/MainActivity.java
+++ app/src/main/java/com/example/jacksonmeyer/firebase/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.jacksonmeyer.firebase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import  com.facebook.login.LoginManager;
+import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
@@ -12,7 +16,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
+        setContentView(R.layout.activity_main);
+
+        if (AccessToken.getCurrentAccessToken() == null) {
+            goLoginScreen();
+        }
+
+    }
+
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void logout(View view) {
+        LoginManager.getInstance().logOut();
+        goLoginScreen();
     }
 }
